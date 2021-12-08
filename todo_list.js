68428dd7145d0760a680bd2f3a1baa0588de5684
fmackercher <68559539+fmackercher@users.noreply.github.com>
2021-12-08 18:12:45 -0500

    Add files via upload

---
+++ todo_list.js
@@ -0,0 +1,69 @@
+
+//const dueTime = document.getElementById('duetime_input').value;
+//const dueDate = document.getElementById('duedate_input').value;
+
+// doneButtonElement.id = '' or setAttribute
+// have a separate add task function from click
+function addTask(description, dueTime) {
+    const newListElement = document.createElement('li');
+    newListElement.textContent = description
+    if (dueTime) {
+        console.log(dueTime)
+        const formatDate = new Date(dueTime);
+        //console.log(formatDate.toDateString());
+        //formatDate.setTime(formatDate.getTime());
+        console.log(formatDate);
+        newListElement.append(' due ');
+        newListElement.append(formatDate.toLocaleString());
+    }
+    task_list.appendChild(newListElement);
+    const doneButtonElement = document.createElement('button');
+    doneButtonElement.textContent = 'Done';
+    doneButtonElement.classList.add('btn', 'btn-sm', 'btn-outline-danger', 'done');
+    newListElement.append(doneButtonElement); // add the button onto the end of the new list element
+    doneButtonElement.onclick = function () {
+        console.log("clicked done");
+        newListElement.remove();
+    }
+}
+
+//Part 3
+function dateAndTimeToTimestamp(dateInputElement, timeInputElement) {
+    const dueDate = dateInputElement.valueAsNumber; // Returns the timestamp at midnight for the given date
+    const dueTime = timeInputElement.valueAsNumber; // Returns the number of milliseconds from midnight to the time
+    if (dueDate && dueTime) { // The user specified both a due date & due time
+        //Add the timezone offset to account for the fact that timestamps are specified by UTC
+        const timezoneOffset = (new Date()).getTimezoneOffset() * 60 * 1000;
+        return dueDate + dueTime + timezoneOffset;
+    } else {
+        // if the user did not specify both a due date and due time, return false
+        return false;
+    }
+}
+
+// addTask('buy milk');
+// () calls function now, addTask is function object that will be called on click
+
+document.getElementById('add_task').onclick = function () {
+    //addTask(description);
+    const description = document.getElementById('task_description_input').value;
+    const dateInputElement = document.getElementById("duedate_input");
+    const timeInputElement = document.getElementById("duetime_input");
+    var dateTime = dateAndTimeToTimestamp(dateInputElement, timeInputElement);
+    console.log(dateInputElement);
+    console.log(timeInputElement);
+    console.log(dateTime);
+    addTask(description, dateTime);
+    document.getElementById("task_description_input").value = "";
+}
+
+
+document.getElementById('task_description_input').addEventListener('keydown', (event) => {
+    const description = document.getElementById('task_description_input').value;
+    const dateInputElement = document.getElementById("duedate_input");
+    const timeInputElement = document.getElementById("duetime_input");
+    var dateTime = dateAndTimeToTimestamp(dateInputElement, timeInputElement);
+    if (event.keyCode == 13) { //event.which == enter
+        addTask(description, dateTime);
+    }
+})
